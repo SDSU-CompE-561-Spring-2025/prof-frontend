cf6f3a39cc04758ed1ba4fc76610d25aaf28de52
Ugur Emre Dogan <ue.dogan@gmail.com>
2025-04-09 20:17:45 -0700

    Created the default next js app.

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+	title: "Budget Buddy",
+	description: "A budgeting app to help you manage your finances",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased text-center text-5xl p-5`}
+			>
+				<h1>NavBar</h1>
+				{children}
+				<footer>Footer</footer>
+			</body>
+		</html>
+	);
+}
