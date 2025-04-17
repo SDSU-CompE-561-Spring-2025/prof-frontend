da042c4df12c33b7313aad4d30f967a0a50d20cb
Ugur Emre Dogan <ue.dogan@gmail.com>
2025-04-16 19:45:01 -0700

    Added Dark mode and dropdown menu for theme switcher button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,22 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import Navbar from '@/components/Navbar';
+import { ThemeProvider } from '@/components/theme-provider';
 
 const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
+	variable: '--font-geist-sans',
+	subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
+	variable: '--font-geist-mono',
+	subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-	title: "Budget Buddy",
-	description: "A budgeting app to help you manage your finances",
+	title: 'Budget Buddy',
+	description: 'A budgeting app to help you manage your finances',
 };
 
 export default function RootLayout({
@@ -23,13 +25,23 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
+		<html
+			lang="en"
+			suppressHydrationWarning
+		>
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased text-center text-5xl p-5`}
 			>
-				<h1>NavBar</h1>
-				{children}
-				<footer>Footer</footer>
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					<Navbar />
+					{children}
+					<footer>Footer</footer>
+				</ThemeProvider>
 			</body>
 		</html>
 	);
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { ThemeSwitcherButton } from '@/components/ThemeSwitcherButton';
+
+function Navbar() {
+	return (
+		<div>
+			<h1>Navbar</h1>
+			<ThemeSwitcherButton />
+		</div>
+	);
+}
+
+export default Navbar;
